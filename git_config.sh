fd6a5cd756eb54e23859f997b7649af5d0b1602a
Alessandro Bellina <abellina@gmail.com>
2014-10-31 22:28:03 -0500

    Fixes completion lookup to use relative directory and creates lg alias for git

--- git_config.sh
+++ git_config.sh
@@ -1,15 +1,13 @@
 #friendly log format (https://coderwall.com/p/euwpig)
-git log\
-    --graph\
-    --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\
-    --abbrev-commit
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 #show branch in command prompt (http://stackoverflow.com/questions/15883416/adding-git-branch-on-the-bash-command-prompt)
 PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
 
 # Source a git completion script
-if [ -f git-completion.sh ]; then
-       . git-completion.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -f $DIR/git-completion.sh ]; then
+       . $DIR/git-completion.sh
 else
   echo "Could not find git completion script"
 fi
